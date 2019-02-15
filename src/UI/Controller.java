6661244dbe45b1288547e77234b9e9ba11ec98c1
sakronos <yangcheng1999@foxmail.com>
2019-02-15 12:00:45 +0800

    2/15 UI first edition

--- src/UI/Controller.java
+++ src/UI/Controller.java
@@ -1,4 +1,26 @@
 package UI;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.*;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.File;
+
 public class Controller {
+    protected String location;
+    @FXML protected TextField show_location;
+    @FXML protected void clickmethod_LocaChoose(ActionEvent event1) {
+
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+        File file = fileChooser.showOpenDialog(new Stage());
+        System.out.println(file);
+    }
 }
