c88c4476860ec9977d6bc399026c669ca3483d73
sakronos <yangcheng1999@foxmail.com>
2019-02-15 17:07:43 +0800

    2/15 Directory Choose is done.

--- src/UI/Controller.java
+++ src/UI/Controller.java
@@ -1,26 +1,23 @@
 package UI;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-import javafx.*;
 import javafx.stage.DirectoryChooser;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-import javax.swing.*;
 import java.io.File;
 
 public class Controller {
-    protected String location;
+    //protected String location;
+    private File file;
     @FXML protected TextField show_location;
-    @FXML protected void clickmethod_LocaChoose(ActionEvent event1) {
-
-        FileChooser fileChooser = new FileChooser();
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
-        fileChooser.getExtensionFilters().add(extFilter);
-        File file = fileChooser.showOpenDialog(new Stage());
-        System.out.println(file);
+    @FXML protected void clickMethod_LocalChoose(ActionEvent event1) {
+        DirectoryChooser directoryChooser=new DirectoryChooser();
+        file = directoryChooser.showDialog(new Stage());
+        try {
+            show_location.setText(file.getPath());
+        } catch (Exception exception) {
+            System.out.println("No Directory is chosen.");
+        }
     }
 }
