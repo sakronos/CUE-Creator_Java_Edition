32f163255271bf0ee3ad8e0e5222eeef70e452a3
sakronos <yangcheng1999@foxmail.com>
2019-02-16 14:34:13 +0800

    2/16 UI is modified.

--- src/UI/Controller.java
+++ src/UI/Controller.java
@@ -8,7 +8,6 @@ import javafx.stage.Stage;
 import java.io.File;
 
 public class Controller {
-    //protected String location;
     private File file;
     @FXML protected TextField show_location;
     @FXML protected void clickMethod_LocalChoose(ActionEvent event1) {
@@ -16,6 +15,7 @@ public class Controller {
         file = directoryChooser.showDialog(new Stage());
         try {
             show_location.setText(file.getPath());
+            /* TODO 显示文件夹内的文件后缀名*/
         } catch (Exception exception) {
             System.out.println("No Directory is chosen.");
         }
--- src/UI/Main.java
+++ src/UI/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
         primaryStage.setTitle("CUE-Creator");
-        primaryStage.setScene(new Scene(root, 600, 500));
+        primaryStage.setScene(new Scene(root, 900, 400));
         primaryStage.show();
     }
 
