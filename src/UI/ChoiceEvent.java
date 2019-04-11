5cf5ab984d65b38a8f3f3bf6bf0519a26f41714d
sakronos <yangcheng1999@foxmail.com>
2019-04-11 22:10:31 +0800

    4/11 添加TTA支持

--- src/UI/ChoiceEvent.java
+++ src/UI/ChoiceEvent.java
@@ -11,6 +11,15 @@ public class ChoiceEvent implements ChangeListener<Number> {
         this.choiceBox = choiceBox;
     }
 
+    /*
+    TODO 现在发现这个类建了没什么用，亏我在这个上面花了那么长时间，
+         不过正是因为研究这个，我了解了JavaFX的监听的写法
+         发现可以自己写Java没有提供的监听功能
+
+    TODO 如果有机会重写这个类，我会将按文件类型限定文件的功能移到这边来，
+         通过传递Files[]数组，这样只需遍历一次文件夹
+     */
+
     @Override
     public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
         switch (choiceBox.getSelectionModel().getSelectedIndex()) {
@@ -31,6 +40,10 @@ public class ChoiceEvent implements ChangeListener<Number> {
                 break;
             case 5:
                 System.out.println("dts");
+                break;
+            case 6:
+                System.out.println("tta");
+                break;
         }
     }
 }
--- src/UI/previewMaker.java
+++ src/UI/previewMaker.java
@@ -19,6 +19,8 @@ public class previewMaker {
                 return ".dsf";
             case 5:
                 return ".dts";
+            case 6:
+                return ".tta";
         }
         return null;
     }
